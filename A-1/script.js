8a7b173142c843f16be451598e40659e65bdcb4d
jeongbin047 <166363537+jeongbin047@users.noreply.github.com>
2024-05-14 10:46:39 +0900

    F-4 menu ing

---
+++ A-1/script.js
@@ -0,0 +1,16 @@
+//슬라이드 메뉴
+$(".main-menu li").mouseover(function(){
+    $(".sub-menu").stop().slideDown();
+}).mouseout(function(){
+    $(".sub-menu").stop().slideUp();
+})
+//공지사항 탭 버튼 클릭했을 때
+$(".tab-title2 ul li:first-child").click(function(){
+   $(".notice-con").show(); //공지사항은 보여주고
+   $(".gallery-con").hide(); //갤러리는 숨긴다.
+})
+//갤러리 탭 버튼 클릭했을 때
+$(".tab-title2 ul li:nth-child(2)").click(function(){
+    $(".notice-con").hide(); //공지사항은 숨기고
+    $(".gallery-con").show(); //갤러리는 보여준다.
+ })
